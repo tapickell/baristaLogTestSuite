bb6a3de059fe5b34c7eed2408babfda18381651f
Todd Pickell <toddpickell@me.com>
2013-03-01 17:08:02 -0600

    setting up step defs to use robotium

--- src/test/java/me/toddpickell/baristalogtest/baristalogstepdefs.java
+++ src/test/java/me/toddpickell/baristalogtest/baristalogstepdefs.java
@@ -1,5 +1,25 @@
 package me.toddpickell.baristalogtest;
 
-public class baristalogstepdefs {
+import com.jayway.android.robotium.solo.Solo;
 
+import android.test.ActivityInstrumentationTestCase2;
+import me.toddpickell.baristalog.MainActivity;
+import cucumber.annotation.*;
+import cucumber.annotation.en.*;
+
+//import org.junit.Assert.assertEquals;
+
+public class baristalogstepdefs extends
+ActivityInstrumentationTestCase2<MainActivity> {
+	
+	private Solo solo;
+	
+	public baristalogstepdefs() {
+		super(MainActivity.class);
+	}
+
+	@Before
+	public void setUp() {
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
 }
